b65937797950bf9b2936d18dcf36c4ab862499ca
yossef ashraf <yossefff2001@gmail.com>
2025-04-12 04:32:26 +0200

    Add method documentation to QuickZoom facade for meeting and user management

--- Facades/QuickZoom.php
+++ Facades/QuickZoom.php
@@ -1,16 +1,26 @@
 <?php
-
-namespace YourVendor\QuickZoom\Facades;
+namespace QuickZoom\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static array listMeetings(string $userId = 'me')
+ * @method static array getMeeting(string $meetingId)
+ * @method static array createMeeting(string $userId = 'me', array $data = [])
+ * @method static array updateMeeting(string $meetingId, array $data = [])
+ * @method static array deleteMeeting(string $meetingId)
+ * @method static array endMeeting(string $meetingId)
+ * @method static array listParticipants(string $meetingId)
+ * @method static array registerParticipant(string $meetingId, array $participantData)
+ * @method static array listRecordings(string $meetingId)
+ * @method static array getUser(string $userId = 'me')
+ * @method static array listUsers()
+ * @method static array createUser(array $userData)
+ * @method static array createWebinar(string $userId, array $data)
+ * @method static array listWebinars(string $userId)
+ */
 class QuickZoom extends Facade
 {
-    /**
-     * Get the registered name of the component.
-     *
-     * @return string
-     */
     protected static function getFacadeAccessor()
     {
         return 'quickzoom';
